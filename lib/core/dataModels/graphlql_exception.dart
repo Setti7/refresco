3eae3bc83f37ae8b89f0423ddafe0fc76d841332
André Setti <ansetti7@gmail.com>
2020-04-09 19:37:24 -0300

    remove unused class

--- lib/core/dataModels/graphlql_exception.dart
+++
@@ -1,7 +0,0 @@
-class GraphQlException implements Exception {
-  String errorMessage;
-  String errorTitle;
-  Exception exception;
-
-  GraphQlException({this.errorTitle, this.errorMessage, this.exception});
-}
